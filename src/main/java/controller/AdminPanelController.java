e85d12a3df6d6363f22c969948c5d17d26531880
Sarkanaaaa <elina.sarkane1@gmail.com>
2021-09-03 16:20:09 +0300

    admin panel changed

--- src/main/java/controller/AdminPanelController.java
+++ src/main/java/controller/AdminPanelController.java
@@ -1,6 +1,8 @@
 package controller;
 import decor.Decor;
 import decor.DecorDBService;
+import events.Event;
+import events.EventDBService;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
@@ -9,10 +11,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Date;
 import java.sql.SQLException;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -22,9 +24,6 @@ import java.util.ResourceBundle;
 public class AdminPanelController extends ViewController implements Initializable {
 
     //calendar sadaļai:
-    public DatePicker calendar;
-    public Button selectButton;
-    public TextField dateFormat;
     public Button backButton;
 
     //decor list sadaļai:
@@ -44,6 +43,21 @@ public class AdminPanelController extends ViewController implements Initializabl
     public TextField decorStatus;
     public Button addButton;
 
+    //update decor sadaļai:
+    public Tab updateDecorPriceTab;
+    public TextField updatePriceField;
+    public Button updatePriceButton;
+    public TableColumn<Object, Object> decorId1;
+    public TableColumn<Object, Object> decorName1;
+    public TableColumn<Object, Object> decorQwt1;
+    public TableColumn<Object, Object> decorPrice1;
+    public TableColumn<Object, Object> decorPriceVat1;
+    public TableColumn<Object, Object> decorStatus1;
+    public TextField newPriceField;
+    public Button updateQuantityButton;
+    public TextField newQuantityField;
+    public TableView<Decor> updateDecorTableList;
+
     //delete decor sadaļai:
     public Tab deleteDecorTab;
     public TableView<Decor> decorListTable;
@@ -55,40 +69,71 @@ public class AdminPanelController extends ViewController implements Initializabl
     public TableColumn<Object, Object> decorStatusColumn1;
     public Button deleteButton;
     public TextField deleteDecorIdField;
-    public Text deleteIdText;
 
-    //update decor Price sadaļai:
-    public TableView<Decor> updatePriceTableList;
-    public Tab updateDecorPriceTab;
-    public TextField updatePriceField;
-    public Button updatePriceButton;
-    public TableColumn<Object, Object> decorId1;
-    public TableColumn<Object, Object> decorName1;
-    public TableColumn<Object, Object> decorQwt1;
-    public TableColumn<Object, Object> decorPrice1;
-    public TableColumn<Object, Object> decorPriceVat1;
-    public TableColumn<Object, Object> decorStatus1;
-    public TextField newPriceField;
+    //Event List sadaļi:
+    public Tab eventListTab;
+    public TableColumn<Object, Object> eventIdColumn;
+    public TableColumn<Object, Object> eventNameColumn;
+    public TableColumn<Object, Object> eventDateColumn;
+    public TableColumn<Object, Object> eventTimeColumn;
+    public TableColumn<Object, Object> eventLocationColumn;
+    public TableColumn<Object, Object> eventGuestQwtColumn;
+    public TextField searchEvent;
+    public TableView<Event> eventListTable;
 
-    //update decor quantity sadaļai:
-    public TableView<Decor> updateDecorQtwTable;
-    public Tab updateDecorQwtTab;
-    public TextField updateQuantityField;
-    public Button updateQuantityButton;
-    public TableColumn<Object, Object> decorId2;
-    public TableColumn<Object, Object> decorName2;
-    public TableColumn<Object, Object> decorQwt2;
-    public TableColumn<Object, Object> decorPrice2;
-    public TableColumn<Object, Object> decorPriceVat2;
-    public TableColumn<Object, Object> decorStatus2;
-    public TextField newQuantityField;
+    //add event sadaļai:
+    public Tab addEventTab;
+    public TextField eventName;
+    public DatePicker eventDate;
+    public TextField eventTime;
+    public TextField eventLocation;
+    public TextField eventGuestQwt;
+    public Button addEventButton;
 
+    //delete event sadaļai:
+    public Tab deleteEventTab;
+    public TextField deleteEventIdField;
+    public TableColumn<Object, Object> eventIdColumn1;
+    public TableColumn<Object, Object> eventNameColumn1;
+    public TableColumn<Object, Object> eventDateColumn1;
+    public TableColumn<Object, Object> eventTimeColumn1;
+    public TableColumn<Object, Object> eventLocationColumn1;
+    public TableColumn<Object, Object> eventGuestQwtColumn1;
+    public Button deleteEventButton;
+    public TableView<Event> deleteEventTable;
+    
+    //update event sadaļai:
+    public Tab updateEventTab;
+    public TextField updateEventIdField;
+    public TextField updateEventName;
+    public TextField updateEventTime;
+    public TextField updateEventLocation;
+    public TextField updateEventQwt;
+    public Button updateNameButton;
+    public Button updateDateButton;
+    public Button updateTimeButton;
+    public Button updateLocationButton;
+    public Button updateGuestQwtButton;
+    public TableView<Event> eventListTable1;
+    public TableColumn<Object, Object> eventIdColumn2;
+    public TableColumn<Object, Object> eventNameColumn2;
+    public TableColumn<Object, Object> eventDateColumn2;
+    public TableColumn<Object, Object> eventTimeColumn2;
+    public TableColumn<Object, Object> eventLocationColumn2;
+    public TableColumn<Object, Object> eventGuestQwtColumn2;
+    public DatePicker updateEventDate;
 
 
     DecorDBService decorDBService = new DecorDBService();
+    EventDBService eventDBService = new EventDBService();
+
     List<Decor> decorList = new ArrayList<>(decorDBService.showAllDecorAdmin());
     ObservableList<Decor> decors = FXCollections.observableArrayList(decorList);
     FilteredList<Decor> filteredDecors = new FilteredList<>(decors, p -> true);
+
+    List<Event> eventList = new ArrayList<>(eventDBService.showAllEvents());
+    ObservableList<Event> events = FXCollections.observableArrayList(eventList);
+    FilteredList<Event> filteredEvents = new FilteredList<>(events, p -> true);
 
 
     public AdminPanelController() throws SQLException {
@@ -97,13 +142,16 @@ public class AdminPanelController extends ViewController implements Initializabl
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         fillInDecorTable();
-        fillInUpdateDecorPriceTable();
-        fillInUpdateDecorQuantityTable();
+        fillInUpdateDecorTable();
         fillInDeleteDecorTable();
-        filterTable();
+        fillInEventListTable();
+        fillInDeleteEventListTable();
+        fillInUpdateEventTable();
+        filterDecorTable();
+        filterEventListTable();
     }
 
-    //delete sadaļa
+    //DECOR SADAĻAS:
     public void fillInDeleteDecorTable(){
         decorIdColumn1.setCellValueFactory(new PropertyValueFactory<>("decorId"));
         decorNameColumn1.setCellValueFactory(new PropertyValueFactory<>("decorName"));
@@ -113,27 +161,15 @@ public class AdminPanelController extends ViewController implements Initializabl
         decorStatusColumn1.setCellValueFactory(new PropertyValueFactory<>("decorStatus"));
         decorListTable.setItems(decors);
     }
-    //update price sadaļa
-    public void fillInUpdateDecorPriceTable(){
+    public void fillInUpdateDecorTable(){
         decorId1.setCellValueFactory(new PropertyValueFactory<>("decorId"));
         decorName1.setCellValueFactory(new PropertyValueFactory<>("decorName"));
         decorQwt1.setCellValueFactory(new PropertyValueFactory<>("decorQwt"));
         decorPrice1.setCellValueFactory(new PropertyValueFactory<>("decorPrice"));
         decorPriceVat1.setCellValueFactory(new PropertyValueFactory<>("decorPriceVAT"));
         decorStatus1.setCellValueFactory(new PropertyValueFactory<>("decorStatus"));
-        updatePriceTableList.setItems(decors);
+        updateDecorTableList.setItems(decors);
     }
-    //update quantity sadaļa
-    public void fillInUpdateDecorQuantityTable(){
-        decorId2.setCellValueFactory(new PropertyValueFactory<>("decorId"));
-        decorName2.setCellValueFactory(new PropertyValueFactory<>("decorName"));
-        decorQwt2.setCellValueFactory(new PropertyValueFactory<>("decorQwt"));
-        decorPrice2.setCellValueFactory(new PropertyValueFactory<>("decorPrice"));
-        decorPriceVat2.setCellValueFactory(new PropertyValueFactory<>("decorPriceVAT"));
-        decorStatus2.setCellValueFactory(new PropertyValueFactory<>("decorStatus"));
-        updateDecorQtwTable.setItems(decors);
-    }
-    //decor list sadaļa
     public void fillInDecorTable() {
         decorIdColumn.setCellValueFactory(new PropertyValueFactory<>("decorId"));
         decorNameColumn.setCellValueFactory(new PropertyValueFactory<>("decorName"));
@@ -143,7 +179,7 @@ public class AdminPanelController extends ViewController implements Initializabl
         decorStatusColumn.setCellValueFactory(new PropertyValueFactory<>("decorStatus"));
         decorTable.setItems(decors);
     }
-    public void filterTable() {
+    public void filterDecorTable() {
         searchTextField.textProperty().addListener((observable, oldValue, newValue) -> {
             filteredDecors.setPredicate(decor -> {
                 if (newValue == null || newValue.isEmpty()) {
@@ -153,6 +189,8 @@ public class AdminPanelController extends ViewController implements Initializabl
                 if (String.valueOf(decor.getDecorName()).toLowerCase().contains(lowerCaseFilter)) {
                     return true;
                 } else if (String.valueOf(decor.getDecorStatus()).toLowerCase().contains(lowerCaseFilter)) {
+                    return true;
+                } else if (String.valueOf(decor.getDecorQwt()).toLowerCase().contains(lowerCaseFilter)) {
                     return true;
                 } else if (String.valueOf(decor.getDecorPriceVAT()).toLowerCase().contains(lowerCaseFilter)) {
                     return true;
@@ -165,16 +203,57 @@ public class AdminPanelController extends ViewController implements Initializabl
         decorTable.setItems(sortedDecor);
     }
 
-
-    //decor lapā ok poga - uz action vajadzētu ievadītos datus aizsūtīt uz sql table decor_list
-    public void handleOkButton(ActionEvent actionEvent) {
-        showAlert("Success", "Decor added Successfully", Alert.AlertType.CONFIRMATION);
+    //EVENT SADAĻAS
+    public void fillInEventListTable(){
+        eventIdColumn.setCellValueFactory(new PropertyValueFactory<>("eventId"));
+        eventNameColumn.setCellValueFactory(new PropertyValueFactory<>("eventName"));
+        eventDateColumn.setCellValueFactory(new PropertyValueFactory<>("dueDate"));
+        eventTimeColumn.setCellValueFactory(new PropertyValueFactory<>("dueTime"));
+        eventLocationColumn.setCellValueFactory(new PropertyValueFactory<>("location"));
+        eventGuestQwtColumn.setCellValueFactory(new PropertyValueFactory<>("guestNumber"));
+        eventListTable.setItems(events);
     }
-
-    //kalendāra izvēles logs
-    public void handleSelectButton(ActionEvent actionEvent) {
-        dateFormat.appendText(calendar.getValue().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) + "\n");
+    public void fillInUpdateEventTable(){
+        eventIdColumn2.setCellValueFactory(new PropertyValueFactory<>("eventId"));
+        eventNameColumn2.setCellValueFactory(new PropertyValueFactory<>("eventName"));
+        eventDateColumn2.setCellValueFactory(new PropertyValueFactory<>("dueDate"));
+        eventTimeColumn2.setCellValueFactory(new PropertyValueFactory<>("dueTime"));
+        eventLocationColumn2.setCellValueFactory(new PropertyValueFactory<>("location"));
+        eventGuestQwtColumn2.setCellValueFactory(new PropertyValueFactory<>("guestNumber"));
+        eventListTable1.setItems(events);
     }
+    public void fillInDeleteEventListTable(){
+        eventIdColumn1.setCellValueFactory(new PropertyValueFactory<>("eventId"));
+        eventNameColumn1.setCellValueFactory(new PropertyValueFactory<>("eventName"));
+        eventDateColumn1.setCellValueFactory(new PropertyValueFactory<>("dueDate"));
+        eventTimeColumn1.setCellValueFactory(new PropertyValueFactory<>("dueTime"));
+        eventLocationColumn1.setCellValueFactory(new PropertyValueFactory<>("location"));
+        eventGuestQwtColumn1.setCellValueFactory(new PropertyValueFactory<>("guestNumber"));
+        deleteEventTable.setItems(events);
+    }
+    public void filterEventListTable(){
+     searchEvent.textProperty().addListener((observable, oldValue, newValue) -> {
+        filteredEvents.setPredicate(event -> {
+            if (newValue == null || newValue.isEmpty()) {
+                return true;
+            }
+            String lowerCaseFilter = newValue.toLowerCase();
+            if (String.valueOf(event.getEventName()).toLowerCase().contains(lowerCaseFilter)) {
+                return true;
+            } else if (String.valueOf(event.getDueDate()).toLowerCase().contains(lowerCaseFilter)) {
+                return true;
+            } else if (String.valueOf(event.getDueTime()).toLowerCase().contains(lowerCaseFilter)) {
+                    return true;
+            } else if (String.valueOf(event.getLocation()).toLowerCase().contains(lowerCaseFilter)) {
+                return true;
+            }
+            return false;
+        });
+    });
+    SortedList<Event> sortedEvent = new SortedList<>(filteredEvents);
+        sortedEvent.comparatorProperty().bind(eventListTable.comparatorProperty());
+    eventListTable.setItems(sortedEvent);
+}
 
     public void handleBackButton(ActionEvent actionEvent) {
         try {
@@ -184,12 +263,11 @@ public class AdminPanelController extends ViewController implements Initializabl
         }
     }
 
-
+    //DECOR METHODS:
     public void handleDeleteButton(ActionEvent actionEvent) throws SQLException {
         decorDBService.deleteDecor(Integer.parseInt(deleteDecorIdField.getText()));
-        showAlert("Success", "Decor deleted successfully", Alert.AlertType.CONFIRMATION);
+        showAlert("Success", "Decor deleted successfully!", Alert.AlertType.CONFIRMATION);
     }
-
     public void handleAddButton(ActionEvent actionEvent) throws SQLException {
         decorDBService.insertNewDecor(
                 decorName.getText(),
@@ -197,22 +275,73 @@ public class AdminPanelController extends ViewController implements Initializabl
                 Double.parseDouble(decorPrice.getText()),
                 decorStatus.getText()
         );
-        showAlert("Success", "Decor added successfully", Alert.AlertType.CONFIRMATION);
+        showAlert("Success", "Decor added successfully!", Alert.AlertType.CONFIRMATION);
     }
-
     public void handleUpdatePriceButton(ActionEvent actionEvent) throws SQLException {
         decorDBService.updateDecorPrice(
                 Integer.parseInt(updatePriceField.getText()),
                 Double.parseDouble(newPriceField.getText())
         );
-        showAlert("Success", "Decor price updated successfully", Alert.AlertType.CONFIRMATION);
+        showAlert("Success", "Decor price updated successfully!", Alert.AlertType.CONFIRMATION);
     }
-
     public void handleUpdateQuantityButton(ActionEvent actionEvent) throws SQLException {
         decorDBService.updateDecorQuantity(
                 Integer.parseInt(updatePriceField.getText()),
                 Integer.parseInt(newQuantityField.getText())
         );
-        showAlert("Success", "Decor quantity updated successfully", Alert.AlertType.CONFIRMATION);
+        showAlert("Success", "Decor quantity updated successfully!", Alert.AlertType.CONFIRMATION);
+    }
+
+    //EVENT T+METHODS:
+    public void handleAddEventButton(ActionEvent actionEvent) throws SQLException {
+        eventDBService.insertNewEvent(
+                eventName.getText(),
+                Date.valueOf(eventDate.getValue()),
+                eventTime.getText(),
+                eventLocation.getText(),
+                Integer.parseInt(eventGuestQwt.getText())
+        );
+        showAlert("Success", "Event added successfully!", Alert.AlertType.CONFIRMATION);
+    }
+    public void handleDeleteEventButton(ActionEvent actionEvent) throws SQLException {
+        eventDBService.deleteEvent(
+                Integer.parseInt(deleteEventIdField.getText())
+        );
+        showAlert("Success", "Event deleted successfully!", Alert.AlertType.CONFIRMATION);
+    }
+    public void handleUpdateEventNameButton(ActionEvent actionEvent) throws SQLException {
+        eventDBService.updateEventName(
+                Integer.parseInt(updateEventIdField.getText()),
+                updateEventName.getText()
+        );
+        showAlert("Success", "Event Name updated successfully!", Alert.AlertType.CONFIRMATION);
+    }
+    public void handleUpdateEventDateButton(ActionEvent actionEvent) throws SQLException {
+        eventDBService.updateEventDate(
+                Integer.parseInt(updateEventIdField.getText()),
+                Date.valueOf(updateEventDate.getValue())
+        );
+        showAlert("Success", "Event Date updated successfully!", Alert.AlertType.CONFIRMATION);
+    }
+    public void handleUpdateEventTimeButton(ActionEvent actionEvent) throws SQLException {
+        eventDBService.updateEventTime(
+                Integer.parseInt(updateEventIdField.getText()),
+                updateEventTime.getText()
+        );
+        showAlert("Success", "Event Time updated successfully!", Alert.AlertType.CONFIRMATION);
+    }
+    public void handleUpdateLocationButton(ActionEvent actionEvent) throws SQLException {
+        eventDBService.updateEventLocation(
+                Integer.parseInt(updateEventIdField.getText()),
+                updateEventLocation.getText()
+        );
+        showAlert("Success", "Event Location updated successfully!", Alert.AlertType.CONFIRMATION);
+    }
+    public void handleUpdateGuestQwtButton(ActionEvent actionEvent) throws SQLException {
+        eventDBService.updateEventGuestQuantity(
+                Integer.parseInt(updateEventIdField.getText()),
+                Integer.parseInt(updateEventQwt.getText())
+        );
+        showAlert("Success", "Event Guest Quantity updated successfully!", Alert.AlertType.CONFIRMATION);
     }
 }
